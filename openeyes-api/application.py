9892665b4698acdaa3a9557a73b23e35896bb86b
OleksandrVakhno <vakhno.olexandrlutsk@gmail.com>
2019-11-02 17:13:44 -0500

    API

--- openeyes-api/application.py
+++ openeyes-api/application.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    from app import api_bp
+    from routes.app import api_bp
     app.register_blueprint(api_bp, url_prefix='/api')
     return app
 
